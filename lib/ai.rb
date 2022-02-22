66fec8e42d7b515ec52c2b6d83a8c53a5fb0bf02
jackvdp <jack@pumpymusic.co.uk>
2022-02-22 16:31:58 +0000

    Started minimax

--- lib/ai.rb
+++ lib/ai.rb
@@ -6,46 +6,89 @@ class AI
 
     def initialize(grid)
         @grid = grid
-        @tictactoe = TicTacToe.new(grid, false)
+        # @tictactoe = TicTacToe.new(grid, false)
     end 
 
-    def make_move(grid)
+
+    # 1
+
+    def check_grid_for_terminal(grid,player_to_play) 
+        tictactoe = TicTacToe.new(grid,player_to_play)
+
+        if tictactoe.verify_win? 
+            return player_to_play ? 10 : -10
+        elsif tictactoe.verify_draw?
+            return 0
+        else
+            return nil
+        end
 
     end
 
-    def get_available_moves
+    # 2
+
+    def get_available_moves(grid)
         available_moves =[]
+
         for i in 0..2 
             for x in 0..2 
-                if @grid[i][x] == ""
-                available_moves.append([i, x])  
+                if grid[i][x] == ""
+                    available_moves.append([i, x])  
                 end
             end
         end 
         return available_moves
     end
 
+    # 3
 
-    def rank_moves(available_moves)
-        move_scores = []
+    def minimax(grid, player_to_play)
 
-        available_moves.each do |move|
-            row = move[0]
-            column = move[1]
+        available_moves = get_available_moves(grid)
 
-            move_result = @tictactoe.attempt_move(row: row, column: column)
-            
-            puts "********* #{move_result}"
+        result = check_grid_for_terminal(grid, player_to_play)
 
-            if move_result == :ai_win
-                move_scores.append(1)
-            else
-                move_scores.append(0)
+        if result != nil
+            return result
+        else 
+
+            moves = []
+
+            for available_moves.each do |move|
+
             end
 
         end
+    end
 
-        return move_scores
-    end 
+
+
+    # def rank_moves(grid)
+    #     move_scores = []
+    #     available_moves = get_available_moves(grid)
+
+    #     available_moves.each do |move|
+    #         tictactoe = TicTacToe.new(grid, false)
+    #         row = move[0]
+    #         column = move[1]
+
+    #         move_result = tictactoe.attempt_move(row: row, column: column)
+            
+    #         if move_result == :ai_win
+    #             move_scores.append(10)
+    #         elsif move_result == :player_win
+    #             move_scores.append(-10)
+    #         elsif move_result == :draw
+    #             move_scores.append(0)
+    #         else 
+    #             move_scores.append(0)
+    #         end
+
+    #     end
+
+    #     return move_scores
+    # end 
+    
+   
 
 end
--- spec/tictactoe_ai_spec.rb
+++ spec/tictactoe_ai_spec.rb
@@ -1,36 +1,112 @@
 require_relative '../lib/ai'
 
 describe AI do
+    
+        # it "gets available moves" do
+        #     grid = [
+        #         ["X","O","O"],
+        #         ["","","O"],
+        #         ["X","O","X"]
+        #     ]
+        #     ai = AI.new(grid)            
+            
+        #     available_moves = ai.get_available_moves(grid)
+        #     expect(available_moves).to eq([[1,0],[1,1]])
+        #     end
+        
+        # it "ranks available moves" do
+        #     # arrange
+        #     grid = [
+        #         ["X","O","O"],
+        #         ["","","O"],
+        #         ["X","O","X"]
+        #     ]
+        #     ai = AI.new(grid)    
+        #     available_moves = [[1,0],[1,1]]
+
+        #     #  act
+        #     move_rankings = ai.rank_moves(grid)
+            
+        #     #  assert 
+
+        #     expect(move_rankings).to eq([0,10])
+       
+        # end
+
+
+
+        # 1
+
+        it "returns the value if the terminal state is found" do 
+            # arrange
+            grid = [
+                ["X","O","O"],
+                ["", "O","X"],
+                ["X","O","X"]
+            ]
+            ai = AI.new(grid)
+           score = ai.check_grid_for_terminal(grid,true)
+
+           expect(score).to eq(10)
+        end
+
+        it "returns the value if the terminal state is found" do 
+            # arrange
+            grid = [
+                ["O","X","X"],
+                ["", "X","O"],
+                ["O","X","O"]
+            ]
+            ai = AI.new(grid)
+           score = ai.check_grid_for_terminal(grid,false)
+
+           expect(score).to eq(-10)
+        end
+
+        it "returns the value if the terminal state is found" do 
+            # arrange
+            grid = [
+                ["O","X","X"],
+                ["X", "O","O"],
+                ["O","X","X"]
+            ]
+            ai = AI.new(grid)
+           score = ai.check_grid_for_terminal(grid,false)
+
+           expect(score).to eq(0)
+        end
+
+        it "returns the value if the terminal state is found" do 
+            # arrange
+            grid = [
+                ["","",""],
+                ["", "",""],
+                ["","",""]
+            ]
+            ai = AI.new(grid)
+           score = ai.check_grid_for_terminal(grid,false)
+
+           expect(score).to eq(nil)
+        end
+
+        # 2
 
         it "gets available moves" do
-            ai = AI.new(
+            grid = [
+                ["","",""],
+                ["","",""],
+                ["","",""]
+            ]
+            ai = AI.new(grid)            
+            
+            available_moves = ai.get_available_moves(grid)
+            expect(available_moves).to eq(
                 [
-                    ["X","O","O"],
-                    ["","","O"],
-                    ["X","O","X"]
+                    [0,0],[0,1], [0,2],
+                    [1,0],[1,1], [1,2],
+                    [2,0],[2,1], [2,2]
                 ]
-            )
-            available_moves = ai.get_available_moves
-            expect(available_moves).to eq([[1,0],[1,1]])
-            end
-        
-        it "ranks available moves" do
-            # arrange
-            ai = AI.new(
-             [
-                ["X","O","O"],
-                ["","","O"],
-                ["X","O","X"]
-            ])
-
-         available_moves = [[1,0],[1,1]]
-
-        #  act
-         move_rankings = ai.rank_moves(available_moves)
-        
-        #  assert 
-
-        expect(move_rankings).to eq([0,1])
-       
+                )
         end
+ 
     end
